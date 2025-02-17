098bdde243d8cda9eb2547450890954e5f3661ba
Alana Braga <alanabragarocha.1802@gmail.com>
2025-02-17 15:38:03 -0300

    Create settings.gradle.kts

---
+++ android/android/settings.gradle.kts
@@ -0,0 +1,25 @@
+Gerenciamento de plugins {
+    val flutterSdkPath =  executar {
+        val propriedades = java.util. Propriedades ()
+        arquivo( " local.properties " ).inputStream().use { propriedades.load(it) }
+        val flutterSdkPath = propriedades.getProperty( " flutter.sdk " )
+        require(flutterSdkPath !=  null ) { " flutter.sdk não definido em local.properties " }
+        CaminhoSdk do Flutter
+    }
+
+    includeBuild( " $flutterSdkPath /pacotes/flutter_tools/gradle " )
+
+    repositórios {
+        Google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+plugins {
+    id( " dev.flutter.flutter-plugin-loader " ) versão " 1.0.0 "
+    id( " com.android.application " ) versão " 8.7.0 " aplicar falso
+    id( " org.jetbrains.kotlin.android " ) versão " 1.8.22 " aplicar falso
+}
+
+incluir( " :app " )
